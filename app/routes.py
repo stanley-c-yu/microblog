f47c1ae79c6af538abd3a6ee785607f6094ba6a7
Stanley Yu <49109653+stanley-c-yu@users.noreply.github.com>
2019-12-23 18:30:51 -0800

    Chapter 3
    Chapter 3 Web Form implementations.

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,8 @@
 #Imports instance of Flask defined as "app" in __init__.py into routes.py
 from flask import render_template
 from app import app
+#Import Login Form Class from forms.py
+from app.forms import LoginForm
 
 #The two @app.route lines of code are Python Decorators.  Decorators are used to modify functions that follow it.  
 #	- In this case, the decorators create an association between the URLs '/' and '/index' and the following function.  
@@ -18,16 +20,19 @@ def index():
     posts = [
         {
             'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
+            'body':'Beautiful day in Portland!'
         },
         {
             'author': {'username': 'Susan'},
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html',title='Home',user=user, posts=posts)
 
- 
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Sign In',form=form)
 #The render_template() function invokes the Jinja2 template engine that comes bundled with the Flask framework. 
 #Jinja2 substitutes {{ ... }} blocks with the corresponding values, given by the arguments provided 
 #in the render_template() call. Note that GitHub Pages doesn't support dynamic elements, so we'll have to axe 
