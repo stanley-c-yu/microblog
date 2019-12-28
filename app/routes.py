aa250e9eb6b2d12a95d21a9cbc3fbaa49c97ad39
Stanley Yu <49109653+stanley-c-yu@users.noreply.github.com>
2019-12-27 21:29:20 -0500

    Updates to Login Form
    Added error messaging and url functions.

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@
 #	  when a client requests a given URL.  
 
 #Imports instance of Flask defined as "app" in __init__.py into routes.py
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 #Import Login Form Class from forms.py
 from app.forms import LoginForm
@@ -29,9 +29,12 @@ def index():
     ]
     return render_template('index.html',title='Home',user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html',title='Sign In',form=form)
 #The render_template() function invokes the Jinja2 template engine that comes bundled with the Flask framework. 
 #Jinja2 substitutes {{ ... }} blocks with the corresponding values, given by the arguments provided 
